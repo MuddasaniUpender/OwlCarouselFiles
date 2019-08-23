a9b771cfa1c2627aa20dc16fc06f60fba7e49cd6
Muddasani Upender Reddy <43833595+MuddasaniUpender@users.noreply.github.com>
2019-08-23 10:45:18 +0530

    Add files via upload

---
+++ owlcarousel/jquery.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:false,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1:{
+            items:1
+        },
+        2:{
+            items:1
+        }
+        
+
+    }
+})
